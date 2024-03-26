24e885bb7159f61e9d3f4b0e77660ee1498f2d3b
Kumar <2090296@cognizant.com>
2024-03-26 20:30:28 +0530

    impl payment service

--- service/payment-service/src/main/java/com/paymentservice/controller/PaymentController.java
+++ service/payment-service/src/main/java/com/paymentservice/controller/PaymentController.java
@@ -1,2 +1,14 @@
-package com.paymentservice.controller;public class PaymentController {
+package com.paymentservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/payment")
+public class PaymentController {
+    @GetMapping("/all")
+    public String retrievePaymentHistory() {
+        return "All Payment";
+    }
 }
