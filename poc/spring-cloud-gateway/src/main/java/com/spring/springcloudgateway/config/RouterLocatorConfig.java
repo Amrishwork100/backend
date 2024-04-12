bf947e80fe5c9f052631e7bd4612a470d9d68490
Kumar <2090296@cognizant.com>
2024-04-12 19:35:34 +0530

    spring cloud gateway

--- poc/spring-cloud-gateway/src/main/java/com/spring/springcloudgateway/config/RouterLocatorConfig.java
+++ poc/spring-cloud-gateway/src/main/java/com/spring/springcloudgateway/config/RouterLocatorConfig.java
@@ -1,2 +1,18 @@
-package com.spring.springcloudgateway.config;public class RouterLocatorConfig {
+package com.spring.springcloudgateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RouterLocatorConfig {
+ /*
+    @Bean
+    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("payment-service", p -> p.path("/api/payment/**")
+                        .uri("http://localhost:9090/"))
+                .build();
+    }*/
 }
--- poc/spring-cloud-gateway/src/main/java/com/spring/springcloudgateway/service/JwtAuthenticationService.java
+++ poc/spring-cloud-gateway/src/main/java/com/spring/springcloudgateway/service/JwtAuthenticationService.java
@@ -1,2 +1,55 @@
-package com.spring.springcloudgateway.service;public class JwtService {
+package com.spring.springcloudgateway.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+import java.util.Map;
+import java.util.function.Function;
+
+@Service
+public class JwtAuthenticationService {
+
+    private static final String SECRET = "AAAAANBUHEHIUHOEHOOBOEBOBOBDNBONOINOIEOIODBUHIUEGYFETCCDSVHEVHBHBEBBBHVDVV";
+
+    public String generateToken(Map<String, Object> claims, String userName) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(userName)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis()+ 5*60*1000))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS512)
+                .compact();
+
+    }
+    public  String extractUserName(String token){
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    private <T> T extractClaim(String token, Function<Claims,T> claimResolver) {
+         Claims claims = extractAllClaims(token);
+        return claimResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+       return Jwts
+               .parserBuilder()
+               .setSigningKey(getSigningKey())
+               .build()
+               .parseClaimsJwt(token)
+               .getBody();
+    }
+
+    private Key getSigningKey() {
+        byte[] secret = Decoders.BASE64.decode(SECRET);
+        return Keys.hmacShaKeyFor(secret);
+
+    }
 }
