957b60b8d6a4c3df92df37389c956904ffd78380
amrishwork100 <amrishwork100@gmail.com>
2024-11-09 20:19:23 +0530

    Authorization server example

---
+++ spring-secuirty/section16/springsecuirtysection16/src/main/java/com/itech/springsecurity/section16/controller/SecureController.java
@@ -0,0 +1,9 @@
+package com.itech.springsecurity.section16.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SecureController {
+
+}
